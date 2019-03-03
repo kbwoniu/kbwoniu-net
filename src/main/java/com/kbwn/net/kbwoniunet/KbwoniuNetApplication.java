cf8930c85a0d150993476cb3d36124c6b0a1d23e
kbwoniu <2274694001@qq.com>
2019-03-03 15:42:10 +0800

    first commit

---
+++ src/main/java/com/kbwn/net/kbwoniunet/KbwoniuNetApplication.java
@@ -0,0 +1,13 @@
+package com.kbwn.net.kbwoniunet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KbwoniuNetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KbwoniuNetApplication.class, args);
+    }
+
+}
