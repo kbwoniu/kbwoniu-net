bc255bf1d8989094fdbe112d17afa3cf2b28b912
kbwoniu <2274694001@qq.com>
2019-03-03 16:15:18 +0800

    第二次修改

--- src/main/java/com/kbwn/net/kbwoniunet/KbwoniuNetApplication.java
+++ src/main/java/com/kbwn/net/kbwoniunet/KbwoniuNetApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class KbwoniuNetApplication {
 
     public static void main(String[] args) {
+        System.out.println("你好旧时光");
         SpringApplication.run(KbwoniuNetApplication.class, args);
     }
 
